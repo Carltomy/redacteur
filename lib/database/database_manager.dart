3fbf71c01f2067d139aaeaf580fa9435b13e3699
Roberto <rprevidence@gmail.com>
2026-01-15 21:29:18 -0500

    correction

--- lib/database/database_manager.dart
+++ lib/database/database_manager.dart
@@ -2,7 +2,6 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import '../modele/redacteur.dart';
 
-
 class DatabaseManager {
   Database? _database;
 
@@ -10,14 +9,15 @@ class DatabaseManager {
     if (_database != null) {
       return _database!;
     }
+
     String path = await getDatabasesPath();
     String fullPath = join(path, 'redacteurs.db');
 
     _database = await openDatabase(
       fullPath,
       version: 1,
-      onCreate: (db, version) {
-        db.execute(
+      onCreate: (db, version) async {
+        await db.execute(
           'CREATE TABLE redacteurs ('
           'id INTEGER PRIMARY KEY AUTOINCREMENT, '
           'nom TEXT, '
@@ -27,13 +27,44 @@ class DatabaseManager {
         );
       },
     );
+
     return _database!;
   }
-  Future<int> insertRedacteur() async {
-    final db = awqit initDatabase();
-    return await db.insert(
+
+  //  AJOUTER
+  Future<int> insertRedacteur(Redacteur redacteur) async {
+    final db = await initDatabase();
+    return await db.insert('redacteurs', redacteur.toMap());
+  }
+
+  // AFFICHER
+  Future<List<Redacteur>> getAllRedacteurs() async {
+    final db = await initDatabase();
+
+    final List<Map<String, dynamic>> maps = await db.query('redacteurs');
+
+    return List.generate(maps.length, (i) {
+      return Redacteur.fromMap(
+        maps[i]['id'],
+        maps[i]['nom'],
+        maps[i]['prenom'],
+        maps[i]['email'],
+      );
+    });
+  }
+
+  Future<int> updateRedacteur(Redacteur redacteur) async {
+    final db = await initDatabase();
+    return await db.update(
       'redacteurs',
       redacteur.toMap(),
+      where: 'id = ?',
+      whereArgs: [redacteur.id],
     );
+  }
+
+  Future<int> deleteRedacteur(int id) async {
+    final db = await initDatabase();
+    return await db.delete('redacteurs', where: 'id = ?', whereArgs: [id]);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,96 @@
 import 'package:flutter/material.dart';
+import 'database/database_manager.dart';
+import 'modele/redacteur.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MonApplication());
 }
 
-class MyApp extends StatefulWidget {
-  const name({super.key});
+class MonApplication extends StatelessWidget {
+  const MonApplication({super.key});
 
-  @override
-  State<name> createState() => _nameState();
-}
-
-class _nameState extends State<name> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title : 'Redacteur',
+        home: const RedacteurInterface(),
+      );
+    );
+  }
+}
+
+class RedacteurInterface extends StatefulWidget {
+  const RedacteurInterface({super.key});
+
+  @override
+  State<RedacteurInterface> createState() => _RedacteurInterfaceState();
+}
+
+class _RedacteurInterfaceState extends State<RedacteurInterface> {
+   final TextEditingController _nomController = TextEditingController();
+  final TextEditingController _prenomController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink,
+        title: const Text('Gestion des rédacteurs',),
+
+      ),
+       body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              controller: _nomController,
+              decoration: const InputDecoration(
+                labelText: 'Nom',
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _prenomController,
+              decoration: const InputDecoration(
+                labelText: 'Prénom',
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+          ],
+          const SizedBox(height: 20,),
+
+          ElevatedButton(
+            onPressed: () async {
+              final nom = _nomController.text;
+              final prenom = _prenomController.text;
+              final email = _emailController.text;
+
+               if (nom.isEmpty || prenom.isEmpty || email.isEmpty) {
+                return;
+            },
+
+            final redacteur = Redacteur(nom, prenom, email);
+
+            await DatabaseManager().insertRedacteur(redacteur);
+
+            _nomController.clear();
+            _prenomController.clear();
+            _emailController.clear();
+
+          )
+        ),
+      ),
+    );
   }
 }
